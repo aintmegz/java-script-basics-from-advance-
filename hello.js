6259bc74c6190d83f3ffcdc07887e945f6272a6b
Megz <chauhanmegh07@gmail.com>
2024-09-06 09:18:19 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+// this ia basic code of java srcipt //
+// console.log is built-in function.
+// that allows you to output messages or values to the console//
+
+
+console.log('Hello World');
+
+// output: hello world //
